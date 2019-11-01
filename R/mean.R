d31ea3f59757b95926d0f4e44ffca591581d2b47
Vincenzo Coia <vincen.coia@stat.ubc.ca>
2019-10-31 19:39:09 -0700

    fix argument of mean()

--- R/mean.R
+++ R/mean.R
@@ -1,6 +1,6 @@
 #' Get the mean of a distribution
 #'
-#' @param object Object of class "dst" to obtain mean from.
+#' @param x Object of class "dst" to obtain mean from.
 #' @param ... Arguments to pass to the \code{integrate()} function.
 #' @param verbose Print output of \code{integrate()} function?
 #' @details If the mean is not already available in the distribution
@@ -8,10 +8,10 @@
 #' function.
 #' @return A single numeric
 #' @export
-mean.dst <- function(object, ..., verbose = FALSE) {
-	mu <- object$prop$mean
+mean.dst <- function(x, ..., verbose = FALSE) {
+	mu <- x$prop$mean
 	if (!is.null(mu)) return(mu)
-	qf <- object$qdst
+	qf <- x$qdst
 	int <- stats::integrate(qf, 0, 1)
 	if (verbose) print(int)
 	int$value
