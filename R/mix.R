7ab9bbcafc7d092db6764b17c60afbd5d75b486d
Vincenzo Coia <vincen.coia@stat.ubc.ca>
2020-03-29 22:17:44 -0700

    expand on ability to probe mixture distributions

--- R/mix.R
+++ R/mix.R
@@ -36,25 +36,78 @@ mix <- function(..., probs) {
 	if (n == 1) {
 		return(dsts[[1]])
 	}
-	res <- list(distributions = dsts,
-				probs = probs)
-	new_dst(res, class = "mix")
+	res <- list(components = list(distributions = dsts,
+								  probs = probs))
+	vvec <- vapply(dsts, variable, FUN.VALUE = character(1))
+	v <- if (all(vvec == vvec[1])) vvec[1] else "mixed"
+	new_dst(res, variable = v, class = "mix")
 }
 
 #' @export
 get_mean.mix <- function(object, ...) {
-	dsts <- object[["distributions"]]
-	p <- object[["probs"]]
-	means <- vapply(dsts, get_mean, numeric(1))
-	sum(p * means)
+	with(object[["components"]], {
+		means <- vapply(distributions, get_mean, FUN.VALUE = numeric(1))
+		sum(probs * means)
+	})
 }
 
 #' @export
 get_variance.mix <- function(object, ...) {
-	dsts <- object[["distributions"]]
-	p <- object[["probs"]]
-	means <- vapply(dsts, get_mean, numeric(1))
-	vars <- vapply(dsts, get_variance, numeric(1))
-	mean <- get_mean(object)
-	sum(p * (vars + means ^ 2 - mean ^ 2))
+	with(object[["components"]], {
+		means <- vapply(distributions, get_mean, FUN.VALUE = numeric(1))
+		overall_mean <- sum(probs * means)
+		variances <- vapply(distributions, get_variance, FUN.VALUE = numeric(1))
+		sum(probs * (variances + means ^ 2 - overall_mean ^ 2))
+	})
+}
+
+#' @export
+get_probfn.mix <- function(object) {
+	if (identical(variable(object), "mixed")) {
+		return(NULL)
+	}
+	with(object[["components"]], {
+		probfns <- lapply(distributions, get_probfn)
+		function(x) {
+			p_times_f <- mapply(function(p, f) p * f(x), probs, probfns,
+								SIMPLIFY = FALSE)
+			Reduce(`+`, p_times_f)
+		}
+	})
+}
+
+#' @export
+get_cdf.mix <- function(object) {
+	with(object[["components"]], {
+		cdfs <- lapply(distributions, get_cdf)
+		function(x) {
+			p_times_cdfs <- mapply(function(p, f) p * f(x), probs, cdfs,
+								   SIMPLIFY = FALSE)
+			Reduce(`+`, p_times_f)
+		}
+	})
+}
+
+#' @export
+get_quantile.mix <- function(object) {
+	stop("Not programmed yet.")
+}
+
+#' @export
+get_randfn.mix <- function(object) {
+	with(object[["components"]], {
+		function(n) {
+			if (n == 0) {
+				if (identical(variable(object), "categorical")) {
+					return(character())
+				} else {
+					return(numeric())
+				}
+			}
+			randfns <- lapply(distributions, get_randfn)
+			k <- length(distributions)
+			id <- sample(1:k, size = n, replace = TRUE, prob = probs)
+			sapply(id, function(i) randfns[[i]](1))
+		}
+	})
 }
