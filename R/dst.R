3b26aef39c77eb26d55b4960930b93849dcc0379
Vincenzo Coia <vincen.coia@stat.ubc.ca>
2020-03-31 15:58:47 -0700

    align plot method argument with generic

--- R/dst.R
+++ R/dst.R
@@ -93,11 +93,11 @@ is.dst <- function(object) inherits(object, "dst")
 #' Plot a functional representation of a distribution.
 #' Wrapper around the \code{graphics::curve} function.
 #'
-#' @param object Distribution object
+#' @param x Distribution object
 #' @param what Name of the representation to plot.
 #' @param ... Other arguments to pass to the \code{graphics::curve} function.
 #' @export
-plot.dst <- function(object,
+plot.dst <- function(x,
 					 what = c("probfn", "cdf", "survival", "quantile", "hazard", "chf"),
 					 ...) {
 	ellipses <- list(...)
@@ -115,22 +115,22 @@ plot.dst <- function(object,
 		if (is.null(ellipses[["xlab"]])) {
 			ellipses[["xlab"]] <- "Probability"
 		}
-		f <- get_quantile(object)
+		f <- get_quantile(x)
 		ellipses[["expr"]] <- as.name("f")
 		do.call(graphics::curve, args = ellipses)
 	}
 	if (is.null(ellipses[["from"]])) {
-		q0 <- eval_quantile(object, at = 0)
+		q0 <- eval_quantile(x, at = 0)
 		if (identical(q0, -Inf)) {
-			ellipses[["from"]] <- eval_quantile(object, at = 0.001)
+			ellipses[["from"]] <- eval_quantile(x, at = 0.001)
 		} else {
 			ellipses[["from"]] <- q0
 		}
 	}
 	if (is.null(ellipses[["to"]])) {
-		q1 <- eval_quantile(object, at = 1)
+		q1 <- eval_quantile(x, at = 1)
 		if (identical(q1, Inf)) {
-			ellipses[["to"]] <- eval_quantile(object, at = 0.999)
+			ellipses[["to"]] <- eval_quantile(x, at = 0.999)
 		} else {
 			ellipses[["to"]] <- q1
 		}
@@ -139,7 +139,7 @@ plot.dst <- function(object,
 		ellipses[["xlab"]] <- "y"
 	}
 	get_fun <- get(paste0("get_", fname))
-	f <- get_fun(object)
+	f <- get_fun(x)
 	ellipses[["expr"]] <- as.name("f")
 	do.call("curve", args = ellipses)
 }
