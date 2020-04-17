8941f0091386738b574dc600cfae7e288b13b693
Vincenzo Coia <vincen.coia@stat.ubc.ca>
2020-04-16 20:55:01 -0700

    skewness computation

--- R/get_skewness.R
+++ R/get_skewness.R
@@ -3,3 +3,15 @@
 #' @param object Distribution object
 #' @export
 get_skewness <- function(object) UseMethod("get_skewness")
+
+#' @export
+get_skewness.dst <- function(object) {
+	mu <- get_mean(object)
+	sigma <- get_sd(object)
+	sf <- get_survival(object)
+	sf2 <- function(x) sf(mu + x ^ (1 / 3))
+	one_minus <- function(x) 1 - sf2(x)
+	pos_int <- integrate(sf2, 0, Inf)
+	neg_int <- integrate(one_minus, -Inf, 0)
+	pos_int$value - neg_int$value
+}
