9c47824a193a4fceef74e1646dfe6b17cda67bad
Vincenzo Coia <vincen.coia@stat.ubc.ca>
2020-04-25 23:11:56 -0700

    change random number generator from a representation to `realise()`

---
+++ R/realise.R
@@ -0,0 +1,23 @@
+#' Generate a Sample from a Distribution
+#'
+#' @param object Object of class "dst"
+#' @param n Number of observations to generate
+#' @return Vector of independent values drawn from the inputted distribution
+#' @rdname realise
+#' @export
+realise <- function(object, n = 1) UseMethod("realise")
+
+#' @rdname realise
+#' @export
+realize <- function(object, n = 1) UseMethod("realise")
+
+#' @export
+realise.dst <- function(object) {
+	r <- object[["representations"]][["fun_rand"]]
+	if (is.null(r)) {
+		u <- stats::runif(n)
+		return(eval_quantile(object, at = u))
+	} else {
+		r
+	}
+}
