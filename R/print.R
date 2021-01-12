f07f4102da1931565c6af02a3fb95b38237bc1fd
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-01-11 21:35:34 -0800

    simplify print method to a more rudimentary display for now (avoid error with tidy eval)

--- R/print.R
+++ R/print.R
@@ -1,17 +1,6 @@
 #' @export
 print.dst <- function(x, ...) {
-	cat(name(x), "Distribution\n")
-	px <- parameters(x)
-	if (!is.null(px)) {
-		cat("\nParameters:\n")
-		df <- data.frame(parameter = names(px),
-						 value = c(px, recursive = TRUE))
-		if (requireNamespace("tibble", quietly = TRUE)) {
-			df <- tibble::as_tibble(df)
-		} else {
-			row.names(df) <- NULL
-		}
-		print(df)
-	}
-	cat("\nNumber of Discontinuities: ", nrow(discontinuities(x)))
+	cat(paste(class(x)))
+	cat("\n\n", names(x[1]), ":\n")
+	print(x[[1]])
 }
