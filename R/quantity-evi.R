d1a736fb20fc541c675500044d29913f1ec8d0cd
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-06-07 13:01:39 -0700

    organize files a bit better

--- R/quantity-evi.R
+++ R/quantity-evi.R
@@ -4,23 +4,13 @@
 #' @param ... Other arguments to pass to specific methods.
 #'
 #' @return A single numeric.
-#' @export
-evi <- function(x, ...) {
-  UseMethod("evi")
-}
-
-#' Extreme Value Index
-#'
-#' @param x Distribution object to obtain EVI from.
-#' @param ... Not used.
-#'
-#' @return A single numeric.
-#'
-#' @details Doesn't calculate EVI if not present in the object.
-#'
 #' @examples
 #' evi(dst_gpd(0, 1, 2))
 #' evi(dst_gpd(0, 1, -1))
 #' evi(dst_unif(0, 10))
 #' evi(dst_norm(5, 5))
 #' evi(dst_empirical(1:10))
+#' @export
+evi <- function(x, ...) {
+  UseMethod("evi")
+}
