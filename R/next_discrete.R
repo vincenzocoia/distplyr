39400c6569ecc6997e0cb9802b83efb5de6203db
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-06-29 15:53:06 -0700

    fix a few things with next_discrete, still relying on the object.

--- R/next_discrete.R
+++ R/next_discrete.R
@@ -1,45 +1,64 @@
+#' Return neighbouring discrete values from a reference
+#'
+#' Finds the next `n` largest/smallest discrete value in a distribution
+#' from some reference point.
+#'
+#' @param object Distribution
+#' @param from Reference value.
+#' @param n Number of discrete values to find.
+#' @return Vector containing the `n` discrete points of non-zero
+#' probability using `from` as the reference point.
 #' @export
-next_discrete <- function(object, at, ...) UseMethod("next_discrete")
+next_discrete <- function(object, from, n = 1L, ...) UseMethod("next_discrete")
 
 #' @export
-next_discrete.dst <- function(object, at, ...) {
+#' @inheritParams next_discrete
+next_discrete.dst <- function(object, from, n = 1L, ...) {
   if (variable(object) == "continuous") {
-    NaN
+    rep(NA_real_, n)
   }
+  error("Cannot find the next discrete value for this distribution.")
 }
 
 #' @export
-next_discrete.pois <- function(object, at, ...) {
-  # check if the number is a whole number
-  if (at < 0) {
-    new_at <- 0
-  } else if (at %% 1 == 0) {
-    new_at <- at + 1
-  } else {
-    new_at <- ceiling(at)
+#' @inheritParams next_discrete
+next_discrete.pois <- function(object, from, n = 1L, ...) {
+  next_discrete_natural(from, n = n)
+}
+
+#' Next/Previous discrete natural number
+#'
+#' Calculates the next/previous natural number (including 0) to
+#' the right/left of `from`.
+#' @inheritParams next_discrete
+next_discrete_natural <- function(from, n = 1L) {
+  if (from < 0) {
+    return(seq_len(n) - 1L)
   }
-  c(new_at)
+  floor(from) + seq_len(n)
 }
 
 #' @export
-next_discrete.finite <- function(object, at, ...) {
-  if (at >= max(object$probabilities$location)) {
-    return(NaN)
-  }
-  next_point <- subset(object$probabilities, location > at, at = at)[1, ]
-  if (is.null(next_point)) {
-    return(NaN)
-  }
-  c(next_point[[1]])
+#' @inheritParams next_discrete
+next_discrete.finite <- function(object, from, n = 1L, ...) {
+  all_discretes <- object$probabilities$location
+  higher_discretes <- all_discretes[all_discretes > from]
+  sort(higher_discretes)[seq_along(n)]
 }
 
 #' @export
-next_discrete.degenerate <- function(object, at, ...) {
+#' @inheritParams next_discrete
+next_discrete.degenerate <- function(object, from, n = 1L, ...) {
   with(parameters(object), {
-    if (at >= location) {
-      NaN
+    if (from >= location) {
+      NA_real_
     } else {
-      c(location)
+      res <- numeric(n)
+      if (n == 0) {
+        return(numeric(0L))
+      } else {
+        c(location, rep(NA_real_, n - 1L))
+      }
     }
   })
 }
