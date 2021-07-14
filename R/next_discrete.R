0eb54376f70db747fc71df6101b6c5e0ecef50de
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-07-14 11:58:47 -0700

    update discrete value finders

--- R/next_discrete.R
+++ R/next_discrete.R
@@ -1,79 +1,97 @@
 #' Return neighbouring discrete values from a reference
 #'
-#' Finds the next `n` largest/smallest discrete value in a distribution
+#' Finds the next `n` largest/smallest discrete values in a distribution
 #' from some reference point.
 #'
 #' @param object Distribution
 #' @param from Reference value.
 #' @param n Number of discrete values to find.
-#' @return Vector containing the `n` discrete points of non-zero
-#' probability using `from` as the reference point.
+#' @param include_from Logical; should the `from` value be included
+#' in the query?
+#' @return Vector of all available discrete points satisfying the query.
+#' If infinite values satify the query, an error is thrown; if less
+#' values are available than asked via `n`, only those values are returned.
 #' @examples
 #' next_discrete(dst_pois(1), from = 1.3)
 #' prev_discrete(dst_pois(1), from = 3, n = 10)
 #' next_discrete(dst_norm(0, 1), from = 1.3, n = 4)
 #' @export
-next_discrete <- function(object, from, n = 1L, ...) UseMethod("next_discrete")
+next_discrete <- function(object, from, n = 1L, include_from = FALSE, ...) {
+  UseMethod("next_discrete")
+}
 
 #' @export
-prev_discrete <- function(object, from, n = 1L, ...) UseMethod("prev_discrete")
+prev_discrete <- function(object, from, n = 1L, include_from = FALSE, ...) {
+  UseMethod("prev_discrete")
+}
 
 #' @export
 #' @inheritParams next_discrete
-next_discrete.dst <- function(object, from, n = 1L, ...) {
+next_discrete.dst <- function(object, from, n = 1L,
+                              include_from = FALSE, ...) {
   if (variable(object) == "continuous") {
-    return(rep(NA_real_, n))
+    return(numeric(0L))
   }
   stop("Cannot find the next discrete values for this distribution.")
 }
 
 #' @export
 #' @inheritParams next_discrete
-prev_discrete.dst <- function(object, from, n = 1L, ...) {
+prev_discrete.dst <- function(object, from, n = 1L,
+                              include_from = FALSE, ...) {
   if (variable(object) == "continuous") {
-    return(rep(NA_real_, n))
+    return(numeric(0L))
   }
   stop("Cannot find the previous discrete values for this distribution.")
 }
 
 #' @export
 #' @inheritParams next_discrete
-next_discrete.pois <- function(object, from, n = 1L, ...) {
-  next_discrete_natural(from, n = n)
+next_discrete.pois <- function(object, from, n = 1L,
+                               include_from = FALSE, ...) {
+  next_discrete_natural(from, n = n, include_from = include_from)
 }
 
 #' @export
 #' @inheritParams next_discrete
-prev_discrete.pois <- function(object, from, n = 1L, ...) {
-  prev_discrete_natural(from, n = n)
+prev_discrete.pois <- function(object, from, n = 1L,
+                               include_from = FALSE, ...) {
+  prev_discrete_natural(from, n = n, include_from = include_from)
 }
 
 #' @export
 #' @inheritParams next_discrete
-next_discrete.finite <- function(object, from, n = 1L, ...) {
+next_discrete.finite <- function(object, from, n = 1L,
+                                 include_from = FALSE, ...) {
   all_discretes <- object$probabilities$location
-  next_discrete_finite(all_discretes, from = from, n = n)
+  next_discrete_finite(all_discretes, from = from, n = n,
+                       include_from = include_from)
 }
 
 #' @export
 #' @inheritParams next_discrete
-prev_discrete.finite <- function(object, from, n = 1L, ...) {
+prev_discrete.finite <- function(object, from, n = 1L,
+                                 include_from = FALSE, ...) {
   all_discretes <- object$probabilities$location
-  prev_discrete_finite(all_discretes, from = from, n = n)
+  prev_discrete_finite(all_discretes, from = from, n = n,
+                       include_from = include_from)
 }
 
 #' @export
 #' @inheritParams next_discrete
-next_discrete.degenerate <- function(object, from, n = 1L, ...) {
+next_discrete.degenerate <- function(object, from, n = 1L,
+                                     include_from = FALSE, ...) {
   x <- parameters(object)[["location"]]
-  next_discrete_finite(x, from = from, n = n)
+  next_discrete_finite(x, from = from, n = n,
+                       include_from = include_from)
 }
 
 #' @export
 #' @inheritParams next_discrete
-prev_discrete.degenerate <- function(object, from, n = 1L, ...) {
+prev_discrete.degenerate <- function(object, from, n = 1L,
+                                     include_from = FALSE, ...) {
   x <- parameters(object)[["location"]]
-  prev_discrete_finite(x, from = from, n = n)
+  prev_discrete_finite(x, from = from, n = n, include_from = include_from)
 }
 
 #' Helper functions for finding discrete values
@@ -85,28 +103,47 @@ prev_discrete.degenerate <- function(object, from, n = 1L, ...) {
 #' @param x A vector of all possible discrete values.
 #' @inheritParams next_discrete
 #' @rdname discrete_helpers
-next_discrete_finite <- function(x, from, n = 1L) {
-  higher_discretes <- x[x > from]
+next_discrete_finite <- function(x, from, n, include_from) {
+  if (include_from) {
+    query <- x >= from
+  } else {
+    query <- x > from
+  }
+  higher_discretes <- x[query]
+  n <- min(length(higher_discretes), n)
   sort(higher_discretes)[seq_len(n)]
 }
 
 #' @rdname discrete_helpers
-prev_discrete_finite <- function(x, from, n = 1L) {
-  lower_discretes <- x[x < from]
+prev_discrete_finite <- function(x, from, n, include_from) {
+  if (include_from) {
+    query <- x <= from
+  } else {
+    query <- x < from
+  }
+  lower_discretes <- x[query]
+  n <- min(length(lower_discretes), n)
   sort(lower_discretes, decreasing = TRUE)[seq_len(n)]
 }
 
 #' @rdname discrete_helpers
-next_discrete_natural <- function(from, n = 1L) {
+next_discrete_natural <- function(from, n, include_from) {
+  if (is.infinite(n)) {
+    stop("Your selection includes and infinite number of discrete points.")
+  }
   if (from < 0) {
     return(seq_len(n) - 1L)
   }
-  floor(from) + seq_len(n)
+  floor_from <- floor(from)
+  adjust <- from == floor_from && include_from
+  floor_from + seq_len(n) - adjust
 }
 
 #' @rdname discrete_helpers
-prev_discrete_natural <- function(from, n = 1L) {
-  res <- ceiling(from) - seq_len(n)
-  res[res < 0] <- NA_real_
-  res
+prev_discrete_natural <- function(from, n, include_from) {
+  ceil_from <- ceiling(from)
+  adjust <- from == ceil_from && include_from
+  n_max <- max(ceil_from + adjust, 0)
+  n <- min(n, n_max)
+  ceil_from - seq_len(n) + adjust
 }
--- tests/testthat/test-next_discrete.R
+++ tests/testthat/test-next_discrete.R
@@ -1,19 +1,48 @@
+test_that("next_discrete_natural works", {
+  expect_equal(next_discrete_natural(5, n = 4, include_from = TRUE), 5:8)
+  expect_equal(next_discrete_natural(5, n = 4, include_from = FALSE), 6:9)
+  expect_equal(next_discrete_natural(5.1, n = 4, include_from = TRUE), 6:9)
+  expect_equal(next_discrete_natural(5.1, n = 4, include_from = FALSE), 6:9)
+  expect_equal(next_discrete_natural(0, n = 4, include_from = TRUE), 0:3)
+  expect_equal(next_discrete_natural(0, n = 4, include_from = FALSE), 1:4)
+  expect_error(next_discrete_natural(5.1, n = Inf, include_from = FALSE))
+  expect_length(next_discrete_natural(5.1, n = 0, include_from = FALSE), 0)
+  expect_length(next_discrete_natural(5.1, n = 0, include_from = TRUE), 0)
+})
+
+test_that("prev_discrete_natural works", {
+  expect_equal(prev_discrete_natural(5, n = 4, include_from = TRUE), 5:2)
+  expect_equal(prev_discrete_natural(5, n = 4, include_from = FALSE), 4:1)
+  expect_equal(prev_discrete_natural(5.1, n = 4, include_from = TRUE), 5:2)
+  expect_equal(prev_discrete_natural(5.1, n = 4, include_from = FALSE), 5:2)
+  expect_equal(prev_discrete_natural(0, n = 4, include_from = TRUE), 0)
+  expect_length(prev_discrete_natural(0, n = 4, include_from = FALSE), 0)
+  expect_equal(prev_discrete_natural(5, n = Inf, include_from = TRUE), 5:0)
+  expect_equal(prev_discrete_natural(5, n = Inf, include_from = FALSE), 4:0)
+  expect_equal(prev_discrete_natural(5.1, n = Inf, include_from = TRUE), 5:0)
+  expect_equal(prev_discrete_natural(5.1, n = Inf, include_from = FALSE), 5:0)
+  expect_length(prev_discrete_natural(5.1, n = 0, include_from = FALSE), 0)
+  expect_length(prev_discrete_natural(5.1, n = 0, include_from = TRUE), 0)
+  expect_length(prev_discrete_natural(-5.1, n = 4, include_from = TRUE), 0)
+  expect_length(prev_discrete_natural(-5.1, n = Inf, include_from = TRUE), 0)
+})
+
 test_that("next_discrete norm works", {
-  expect_equal(NA_real_, next_discrete(dst_norm(1, 2), 1))
-  expect_equal(NA_real_, next_discrete(dst_norm(12, 1), -2))
-  expect_equal(NA_real_, next_discrete(dst_norm(65, 1), 0))
+  expect_equal(numeric(), next_discrete(dst_norm(1, 2), 1))
+  expect_equal(numeric(), next_discrete(dst_norm(12, 1), -2, n = 10))
+  expect_equal(numeric(), next_discrete(dst_norm(65, 1), 0, n = Inf))
 })
 
 test_that("next_discrete gpd works", {
-  expect_equal(NA_real_, next_discrete(dst_gpd(1, 2, -4), 1))
-  expect_equal(NA_real_, next_discrete(dst_gpd(1, 1, 2), 1546))
-  expect_equal(NA_real_, next_discrete(dst_gpd(561, 1, 8), -565))
+  expect_equal(numeric(), next_discrete(dst_gpd(1, 2, -4), 1))
+  expect_equal(numeric(), next_discrete(dst_gpd(1, 1, 2), 1546, n = 10))
+  expect_equal(numeric(), next_discrete(dst_gpd(561, 1, 8), -565, n = Inf))
 })
 
 test_that("next_discrete unif works", {
-  expect_equal(NA_real_, next_discrete(dst_unif(1, 2), 1))
-  expect_equal(NA_real_, next_discrete(dst_unif(-15, 0), -6))
-  expect_equal(NA_real_, next_discrete(dst_unif(561, 18000), -99))
+  expect_equal(numeric(), next_discrete(dst_unif(1, 2), 1))
+  expect_equal(numeric(), next_discrete(dst_unif(-15, 0), -6, n = 10))
+  expect_equal(numeric(), next_discrete(dst_unif(561, 18000), -99, n = Inf))
 })
 
 test_that("next_discrete pois works", {
@@ -40,18 +69,18 @@ test_that("next_discrete pois works", {
 })
 
 test_that("next_discrete degenerate works", {
-  expect_equal(NA_real_, next_discrete(dst_degenerate(1), 1))
+  expect_equal(numeric(), next_discrete(dst_degenerate(1), 1))
   expect_equal(-15, next_discrete(dst_degenerate(-15), -16))
-  expect_equal(NA_real_, next_discrete(dst_degenerate(56), 80))
+  expect_equal(numeric(), next_discrete(dst_degenerate(56), 80))
   expect_equal(56, next_discrete(dst_degenerate(56), 16))
 })
 
 
 test_that("next_discrete lnorm works", {
-  expect_equal(NA_real_, next_discrete(dst_lnorm(1, 2), 1))
-  expect_equal(NA_real_, next_discrete(dst_lnorm(-15, 2), -16))
-  expect_equal(NA_real_, next_discrete(dst_lnorm(2, 3), 80))
-  expect_equal(NA_real_, next_discrete(dst_lnorm(2, 2), 16))
+  expect_equal(numeric(), next_discrete(dst_lnorm(1, 2), 1))
+  expect_equal(numeric(), next_discrete(dst_lnorm(-15, 2), -16))
+  expect_equal(numeric(), next_discrete(dst_lnorm(2, 3), 80))
+  expect_equal(numeric(), next_discrete(dst_lnorm(2, 2), 16))
 })
 
 car <- data.frame(
@@ -72,7 +101,7 @@ test_that("next_discrete finite works", {
     next_discrete(dst_empirical(hp, data = car), 95)
   )
   expect_equal(
-    NA_real_,
+    numeric(),
     next_discrete(dst_empirical(hp, data = car), 335)
   )
   expect_equal(
@@ -84,7 +113,7 @@ test_that("next_discrete finite works", {
     next_discrete(dst_finite(1:5, rep(0.2, 5)), 0)
   )
   expect_equal(
-    NA_real_,
+    numeric(),
     next_discrete(dst_finite(1:5, rep(0.2, 5)), 5)
   )
   expect_equal(
@@ -92,7 +121,7 @@ test_that("next_discrete finite works", {
     next_discrete(dst_finite(1:5, rep(0.2, 5)), 0.3235)
   )
   expect_equal(
-    NA_real_,
+    numeric(),
     next_discrete(dst_finite(1:5, rep(0.2, 5)), 65.265)
   )
 })
