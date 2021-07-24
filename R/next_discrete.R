a9a9b3d11fe8e40b30fc8bb6667e3fd862b673fa
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-07-23 22:54:17 -0700

    code up first draft of inverse distribution breakpoints

--- R/next_discrete.R
+++ R/next_discrete.R
@@ -94,6 +94,98 @@ prev_discrete.degenerate <- function(object, from, n = 1L,
   prev_discrete_finite(x, from = from, n = n, include_from = include_from)
 }
 
+#' @export
+#' @inheritParams next_discrete
+next_discrete.inverse <- function(object, from, n, include_from) {
+  d_nested <- object$distribution
+  if (from >= 0) {
+    n_available <- num_discretes(d_nested, from = 0, to = 1 / from,
+                                 include_from = FALSE,
+                                 include_to = include_from)
+    n <- min(n, n_available)
+    x <- prev_discrete(d_nested, from = 1 / from, n = n,
+                       include_from = include_from)
+  } else {
+    x <- prev_discrete(d_nested, from = 1 / from, n = n,
+                           include_from = include_from)
+    n_x <- length(x)
+    n_remaining <- n - n_x
+    if (n_remaining) {
+      n_pos <- num_discretes(d_nested, from = 0, to = Inf,
+                             include_from = FALSE,
+                             include_to = FALSE)
+      n_remaining <- min(n_remaining, n_pos)
+      x_pos <- prev_discretes(d_nested, from = Inf, n = n_remaining,
+                              include_from = FALSE)
+      x <- c(x, x_pos)
+    }
+  }
+  1 / x
+}
+
+#' @export
+#' @inheritParams next_discrete
+prev_discrete.inverse <- function(object, from, n, include_from) {
+  d_nested <- object$distribution
+  if (from <= 0) {
+    n_available <- num_discretes(d_nested, from = -1 / abs(from), to = 0,
+                                 include_from = include_from,
+                                 include_to = FALSE)
+    n <- min(n, n_available)
+    x <- next_discrete(d_nested, from = -1 / abs(from), n = n,
+                       include_from = include_from)
+  } else {
+    x <- next_discrete(d_nested, from = 1 / from, n = n,
+                       include_from = include_from)
+    n_x <- length(x)
+    n_remaining <- n - n_x
+    if (n_remaining) {
+      n_neg <- num_discretes(d_nested, from = -Inf, to = 0,
+                             include_from = FALSE,
+                             include_to = FALSE)
+      n_remaining <- min(n_remaining, n_neg)
+      x_neg <- next_discretes(d_nested, from = -Inf, n = n_remaining,
+                              include_from = FALSE)
+      x <- c(x, x_neg)
+    }
+  }
+  1 / x
+}
+
+#' @export
+num_discretes.inverse <- function(object, from, to, include_from, include_to) {
+  d_nested <- object$distribution
+  if (to < from) {
+    a <- to
+    b <- from
+    include_a <- include_to
+    include_b <- include_from
+  } else {
+    a <- from
+    b <- to
+    include_a <- include_from
+    include_b <- include_to
+  }
+  if (b <= 0) {
+    n <- num_discretes(d_nested, from = -1 / abs(b), to = -1 / abs(a),
+                       include_from = include_b,
+                       include_to = include_a)
+  } else if (a >= 0) {
+    n <- num_discretes(d_nested, from = 1 / b, to = 1 / a,
+                       include_from = include_b,
+                       include_to = include_a)
+  } else {
+    n_neg <- num_discretes(d_nested, from = -Inf, to = 1 / a,
+                           include_from = FALSE,
+                           include_to = include_a)
+    n_pos <- num_discretes(d_nested, from = 1 / b, to = Inf,
+                           include_from = include_b,
+                           include_to = FALSE)
+    n <- n_neg + n_pos
+  }
+  n
+}
+
 #' Helper functions for finding discrete values
 #'
 #' `*_discrete_finite` finds discrete values from a
@@ -128,6 +220,9 @@ prev_discrete_finite <- function(x, from, n, include_from) {
 
 #' @rdname discrete_helpers
 next_discrete_natural <- function(from, n, include_from) {
+  if (from == Inf) {
+    return(numeric(0L))
+  }
   if (is.infinite(n)) {
     stop("Your selection includes and infinite number of discrete points.")
   }
@@ -141,6 +236,9 @@ next_discrete_natural <- function(from, n, include_from) {
 
 #' @rdname discrete_helpers
 prev_discrete_natural <- function(from, n, include_from) {
+  if (n > 0 && from == Inf) {
+    stop("Impossible to find a discrete value neighbouring infinity.")
+  }
   ceil_from <- ceiling(from)
   adjust <- from == ceil_from && include_from
   n_max <- max(ceil_from + adjust, 0)
