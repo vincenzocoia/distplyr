5e4793fb9893ba6f70fa4f48375dc87dc1c503f0
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-07-27 13:42:36 -0400

    Make mutate_parameters

---
+++ R/mutate_parameters.R
@@ -0,0 +1,26 @@
+#' Mutate Parameters
+#'
+#' Modify parameters using tidy evaluation. Lightweight function that only
+#' checks that the parameter you're modifying already exists in the
+#' distribution.
+#' @param distribution Parametric distribution
+#' @param ... Named expressions. Names should be parameters names of the
+#' distribution. Expressions can involve computations with other parameters.
+#' @return The input distribution, with the parameters modified as specified
+#' in `...`.
+#' @examples
+#' d <- dst_unif(1, 3)
+#' distplyr:::mutate_parameters(d, min = max - min)
+mutate_parameters <- function(distribution, ...) {
+	ell <- rlang::enquos(...)
+	params <- parameters(distribution)
+	param_names <- names(params)
+	for (i in seq_along(ell)) {
+		param_name <- names(ell)[[i]]
+		stopifnot(param_name %in% param_names)
+		param_val <- ell[[i]]
+		new_val <- rlang::eval_tidy(param_val, data = params)
+		distribution$parameters[[param_name]] <- new_val
+	}
+	distribution
+}
