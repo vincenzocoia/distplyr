7fc76e8579722405b3d731b18dda2e5cb534ebb6
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-07-30 15:59:27 -0400

    move mix methods to separate files

---
+++ R/quantities-mix.R
@@ -0,0 +1,90 @@
+#' @export
+mean.mix <- function(x, ...) {
+	with(x[["components"]], {
+		means <- vapply(distributions, mean, FUN.VALUE = numeric(1L))
+		sum(probs * means)
+	})
+}
+
+#' @export
+variance.mix <- function(x, ...) {
+	overall_mean <- mean(x)
+	with(x[["components"]], {
+		means <- vapply(distributions, mean, FUN.VALUE = numeric(1L))
+		variances <- vapply(distributions, variance, FUN.VALUE = numeric(1L))
+		sum(probs * (variances + means^2 - overall_mean^2))
+	})
+}
+
+#' @export
+skewness.mix <- function(x, ...) {
+	overall_mean <- mean(x)
+	overall_sd <- stdev(x)
+	with(x[["components"]], {
+		means <- vapply(distributions, mean, FUN.VALUE = numeric(1L))
+		vars <- vapply(distributions, variance, FUN.VALUE = numeric(1L))
+		sds <- sqrt(vars)
+		skews <- vapply(distributions, skewness, FUN.VALUE = numeric(1L))
+		cmoms <- list(
+			zero = 1,
+			first = 0,
+			second = vars,
+			third = skews * sds^3
+		)
+		terms <- lapply(0:3, function(k) {
+			choose(3, k) * (means - overall_mean)^(3 - k) * cmoms[[k + 1L]]
+		})
+		sum(probs * Reduce(`+`, terms)) / overall_sd^3
+	})
+}
+
+#' @export
+kurtosis_exc.mix <- function(x, ...) {
+	overall_mean <- mean(x)
+	overall_var <- variance(x)
+	with(x[["components"]], {
+		means <- vapply(distributions, mean, FUN.VALUE = numeric(1L))
+		vars <- vapply(distributions, variance, FUN.VALUE = numeric(1L))
+		sds <- sqrt(vars)
+		skews <- vapply(distributions, skewness, FUN.VALUE = numeric(1L))
+		kurts <- vapply(distributions, kurtosis_raw, FUN.VALUE = numeric(1L))
+		cmoms <- list(
+			zero = 1,
+			first = 0,
+			second = vars,
+			third = skews * sds^3,
+			fourth = vars^2 * kurts
+		)
+		terms <- lapply(0:4, function(k) {
+			choose(4, k) * (means - overall_mean)^(4 - k) * cmoms[[k + 1L]]
+		})
+		sum(probs * Reduce(`+`, terms)) / overall_var^2 - 3
+	})
+}
+
+#' @export
+evi.mix <- function(x, ...) {
+	if (is_finite_dst(x)) {
+		return(NaN)
+	}
+	with(x[["components"]], {
+		right_ends <- vapply(distributions, eval_quantile,
+							 at = 1,
+							 FUN.VALUE = numeric(1L)
+		)
+		max_end <- max(right_ends)
+		has_max_ends <- right_ends == max_end
+		evis <- vapply(distributions, evi, FUN.VALUE = numeric(1L))
+		final_sign <- if (max_end < Inf) -1 else 1
+		final_sign * max(abs(evis[has_max_ends]))
+	})
+}
+
+
+#' @export
+range.mix <- function(object, ...) {
+	r <- lapply(object$components$distributions, range)
+	low <- Reduce(min, r)
+	high <- Reduce(max, r)
+	c(low, high)
+}
---
+++ R/representations-mix.R
@@ -0,0 +1,78 @@
+#' @export
+eval_cdf.mix <- function(object, at) {
+	with(object[["components"]], {
+		cdf_vals <- lapply(distributions, eval_cdf, at = at)
+		p_times_cdfs <- mapply(function(p, f) p * f, probs, cdf_vals,
+							   SIMPLIFY = FALSE
+		)
+		Reduce(`+`, p_times_cdfs)
+	})
+}
+
+#' @export
+eval_quantile.mix <- function(object, at, tol = 1e-6, maxiter = 1000, ...) {
+	distributions <- object[["components"]][["distributions"]]
+	cdf <- get_cdf(object)
+	discon <- discontinuities(object)
+	res <- at
+	ones <- vapply(at == 1, isTRUE, FUN.VALUE = logical(1L))
+	if (any(ones)) {
+		right_ends <- lapply(distributions, eval_quantile, at = 1)
+		res[ones] <- do.call(max, right_ends)
+	}
+	res[!ones] <- eval_quantile_from_cdf(
+		cdf, discon,
+		at = at[!ones], tol = tol, maxiter = maxiter
+	)
+	res
+}
+
+#' @export
+eval_pmf.mix <- function(object, at, strict = TRUE, ...) {
+	with(object$components, {
+		cumulitive_sum <- 0
+		for (i in 1:length(distributions)) {
+			tryCatch(
+				{
+					cumlative_sum <- cumulitive_sum +
+						eval_pmf(distributions[[i]], at, strict = strict) * probs[[i]]
+				},
+				error = function(c) {
+					warning("A component distribution doesn't have a pmf. Perhaps you want to evaluate in non-strict mode?")
+					return(NA)
+				}
+			)
+		}
+		cumulitive_sum
+	})
+}
+
+#' @export
+eval_density.mix <- function(object, at, strict = TRUE) {
+	if (variable(object) != "continuous") {
+		return(NULL)
+	}
+	with(object[["components"]], {
+		density_vals <- lapply(distributions, eval_density, at = at)
+		p_times_f <- mapply(function(p, f) p * f, probs, density_vals,
+							SIMPLIFY = FALSE
+		)
+		Reduce(`+`, p_times_f)
+	})
+}
+
+#' @export
+realise.mix <- function(object, n = 1, ...) {
+	with(object[["components"]], {
+		if (n == 0) {
+			if (identical(variable(object), "categorical")) {
+				return(character())
+			} else {
+				return(numeric())
+			}
+		}
+		k <- length(distributions)
+		id <- sample(1:k, size = n, replace = TRUE, prob = probs)
+		sapply(id, function(i) realise(distributions[[i]]))
+	})
+}
