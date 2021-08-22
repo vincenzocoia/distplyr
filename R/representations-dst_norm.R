aa2796a5bc6b845594c6ace7c4772f3dcf545dd8
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-08-21 19:00:11 -0700

    remove sd from list of parameters in dst_norm

--- R/representations-dst_norm.R
+++ R/representations-dst_norm.R
@@ -1,34 +1,34 @@
 #' @export
 eval_cdf.norm <- function(object, at) {
 	with(parameters(object), {
-		stats::pnorm(at, mean = mean, sd = sd)
+		stats::pnorm(at, mean = mean, sd = sqrt(variance))
 	})
 }
 
 #' @export
 eval_survival.norm <- function(object, at) {
 	with(parameters(object), {
-		stats::pnorm(at, mean = mean, sd = sd, lower.tail = FALSE)
+		stats::pnorm(at, mean = mean, sd = sqrt(variance), lower.tail = FALSE)
 	})
 }
 
 #' @export
 eval_density.norm <- function(object, at, strict = TRUE) {
 	with(parameters(object), {
-		stats::dnorm(at, mean = mean, sd = sd)
+		stats::dnorm(at, mean = mean, sd = sqrt(variance))
 	})
 }
 
 #' @export
 realise.norm <- function(object, n = 1, ...) {
 	with(parameters(object), {
-		stats::rnorm(n, mean = mean, sd = sd)
+		stats::rnorm(n, mean = mean, sd = sqrt(variance))
 	})
 }
 
 #' @export
 eval_quantile.norm <- function(object, at, ...) {
 	with(parameters(object), {
-		stats::qnorm(at, mean = mean, sd = sd)
+		stats::qnorm(at, mean = mean, sd = sqrt(variance))
 	})
 }
