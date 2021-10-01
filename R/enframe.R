5cdd7e3710bec07bbea653138aa23a8825d5d284
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-10-01 15:54:01 -0700

    some old work

--- R/enframe.R
+++ R/enframe.R
@@ -1,6 +1,6 @@
 #' @export
-enframe_general <- function(..., eval_fn, at, arg_name, fn_name,
-							fn_args = list()) {
+enframe_general <- function(..., eval_fn, at, arg_name, fn_prefix,
+							names_repair, fn_args = list()) {
 	ellipsis <- rlang::quos(...)
 	distributions <- lapply(ellipsis, rlang::eval_tidy)
 	n <- length(distributions)
@@ -8,20 +8,15 @@ enframe_general <- function(..., eval_fn, at, arg_name, fn_name,
 	for (i in seq_len(n)) {
 		f[[i]] <- rlang::exec(
 			eval_fn, object = distributions[[i]], at = at, !!!fn_args
-		) #object = distributions[[i]], !!!fn_args)
+		)
 	}
-	if (n == 1L) {
-		names(f) <- fn_name
-	} else {
-		for (i in 1:n) {
-			if (rlang::quo_is_symbol(ellipsis[[i]])) {
-				sym <- rlang::as_name(ellipsis[[i]])
-				names(f)[i] <- paste0(fn_name, ".", sym)
-			} else {
-				names(f)[i] <- paste0(fn_name, "..", i)
-			}
-		}
-	}
+	ellipsis_char <- vctrs::vec_cast(ellipsis, to = character())
+	names_method <- match(names_repair)
+	ellipsis_as_names <- vctrs::vec_as_names(
+		ellipsis_char, repair = names_method, quiet = TRUE
+	)
+	names_with_fn <- paste0(fn_prefix, "_", ellipsis_as_names)
+	names(f) <- names_with_fn
 	arg <- list(at)
 	names(arg) <- arg_name
 	res <- as.data.frame(c(arg, f))
@@ -30,16 +25,21 @@ enframe_general <- function(..., eval_fn, at, arg_name, fn_name,
 
 #' @rdname cdf
 #' @export
-enframe_cdf <- function(..., at, arg_name = ".arg", fn_name = ".cdf") {
+enframe_cdf <- function(..., at, arg_name = ".arg", fn_prefix = "cdf",
+						names_repair = c("universal", "unique", "minimal")) {
 	enframe_general(..., eval_fn = eval_cdf, at = at,
-					arg_name = arg_name, fn_name = fn_name)
+					arg_name = arg_name, fn_prefix = fn_prefix,
+					names_repair = names_repair)
 }
 
 #' @rdname pmf
 #' @export
-enframe_pmf <- function(..., at, arg_name = ".arg", fn_name = ".pmf",
+enframe_pmf <- function(..., at, arg_name = ".arg", fn_prefix = "pmf",
+						names_repair = c("universal", "unique", "minimal"),
 						strict = TRUE) {
 	enframe_general(..., eval_fn = eval_pmf, at = at,
-					arg_name = arg_name, fn_name = fn_name,
+					arg_name = arg_name,
+					fn_prefix = fn_prefix,
+					names_repair = names_repair,
 					fn_args = list(strict = strict))
 }
