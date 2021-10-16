bf04c4ca5e19b26599f115e792f11a7f7c7096e1
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-10-15 23:39:51 -0700

    begin migrating parametric distributions to new format.

--- R/dst_pois.R
+++ R/dst_pois.R
@@ -2,18 +2,15 @@
 #'
 #' Makes a distribution belonging to the family of
 #' Poisson distributions.
-#' @param lambda Rate of Occurance for distribution
+#' @param lambda Mean of the Poisson distribution.
 #' @return Object of class "dst".
-#' dst_poisson(1)
+#' dst_pois(1)
 #' @export
 dst_pois <- function(lambda) {
-  if (lambda < 0) {
-    stop("'lambda' parameter must greater than 0")
-  } else if (lambda == 0) {
-    return(dst_degenerate(lambda))
-  }
-  res <- list(parameters = list(
-    lambda = lambda
-  ))
-  new_parametric(res, variable = "discrete", class = "pois")
+	if (lambda < 0) {
+		stop("'lambda' parameter must greater than 0")
+	} else if (lambda == 0) {
+		return(dst_degenerate(lambda))
+	}
+	dst_parametric("pois", lambda = lambda, .variable = "discrete")
 }
