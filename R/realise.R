1abea26f897238def9450e1db58a025bb18e365d
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-10-20 08:33:08 -0700

    Make `observe()` alias to `realise()`.

--- R/realise.R
+++ R/realise.R
@@ -1,16 +1,15 @@
 #' Generate a Sample from a Distribution
 #'
+#' Draw `n` independent observations from a distribution.
+#'
 #' @param distribution Distribution.
 #' @param n Number of observations to generate
 #' @param ... Other arguments to pass to \code{\link{eval_quantile}}
 #' @return Vector of independent values drawn from the inputted distribution
+#' @note `realise()`, `realize()`, and `observe()` all do the same thing.
 #' @rdname realise
 #' @export
 realise <- function(distribution, n = 1, ...) UseMethod("realise")
-
-#' @rdname realise
-#' @export
-realize <- function(distribution, n = 1, ...) UseMethod("realise")
 
 #' @export
 realise.dst <- function(distribution, n = 1, ...) {
@@ -18,7 +17,14 @@ realise.dst <- function(distribution, n = 1, ...) {
   return(eval_quantile(distribution, at = u, ...))
 }
 
+#' @rdname realise
 #' @export
-realise.default <- function(distribution, n = 1, ...) {
-  rep(distribution, n)
+realize <- function(distribution, n = 1) {
+	realise(distribution, n = n)
+}
+
+#' @rdname realise
+#' @export
+observe <- function(distribution, n = 1) {
+	realise(distribution, n = n)
 }
