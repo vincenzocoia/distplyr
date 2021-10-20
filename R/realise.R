e4844a8872f1b838eb38b18d7a7a5c45a8e50d96
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-10-20 08:44:53 -0700

    Elaborate on the rationale for observe()

--- R/realise.R
+++ R/realise.R
@@ -7,6 +7,8 @@
 #' @param ... Other arguments to pass to \code{\link{eval_quantile}}
 #' @return Vector of independent values drawn from the inputted distribution
 #' @note `realise()`, `realize()`, and `observe()` all do the same thing.
+#' Although the language `observe()` is more common, it's only an alias here,
+#' because this function name is common amongst other packages.
 #' @rdname realise
 #' @export
 realise <- function(distribution, n = 1, ...) UseMethod("realise")
