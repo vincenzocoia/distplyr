d40e4fd4cccd963214e1efe68fa1e257d42e7c06
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-10-21 22:15:35 -0700

    Update representations-graft.R

--- R/representations-graft.R
+++ R/representations-graft.R
@@ -6,11 +6,11 @@ eval_quantile.graft <- function(x, at, ...) {
 	for (i in seq_along(at)) {
 		if (at[i] <= p_cutoff) {
 			new_at <- at[i] / p_cutoff
-			this_d <- d$components$distributions[[1L]]
+			this_d <- x$components$distributions[[1L]]
 			res[i] <- eval_quantile(this_d, at = new_at)
 		} else {
 			new_at <- (at[i] - p_cutoff) / (1 - p_cutoff)
-			this_d <- d$components$distributions[[2L]]
+			this_d <- x$components$distributions[[2L]]
 			res[i] <- eval_quantile(this_d, at = new_at)
 		}
 	}
