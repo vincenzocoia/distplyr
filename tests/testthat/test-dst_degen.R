f1ef1e53bc157e1698bc3d762dc557369a11c56f
Vincenzo Coia <vincenzo.coia@gmail.com>
2021-11-05 15:39:21 -0700

    move out most tests for now.

--- tests/testthat/test-dst_degen.R
+++
@@ -1,9 +0,0 @@
-test_that("Distribution quantities are appropriate", {
-  .dst <- dst_degenerate(0.5)
-  expect_equal(eval_pmf(.dst, -10:10), rep(0, 21))
-  expect_equal(eval_pmf(.dst, 0.5), 1)
-  expect_equal(eval_cdf(.dst, -10:10), c(rep(0, 11), rep(1, 10)))
-  expect_equal(eval_cdf(.dst, 0.5), 1)
-  expect_equal(realise(.dst, 10), rep(0.5, 10))
-  expect_equal(eval_quantile(.dst, 1:9 / 10), rep(0.5, 9))
-})
--- tests/testthat/test-plateaus.R
+++
@@ -1,9 +0,0 @@
-x <- 1:5
-y <- c(1, 3, 2, Inf, 4, -Inf)
-fr <- stepfun(x, y, right = TRUE)
-fl <- stepfun(x, y, right = FALSE)
-
-test_that("plateaus works", {
-  expect_equal(plateaus(fr), y)
-  expect_equal(plateaus(fl), y)
-})
