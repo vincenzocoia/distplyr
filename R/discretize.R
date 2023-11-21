583f7b340707732c01ba7e075c614bb95cdd4049
Vincenzo Coia <vincenzo.coia@gmail.com>
2023-11-21 14:01:44 -0800

    allow manual specification of discretize bin values

--- R/discretize.R
+++ R/discretize.R
@@ -5,9 +5,15 @@
 #'
 #' @param distribution Distribution to discretise.
 #' @param breakpoints Vector of breakpoints separating each bin.
-#' @param midpoints How to determine value for each bin.
+#' @param midpoints,values Specify the values associated with each new bin
+#' by either specifying a computation of the `midpoint`, or as a vector
+#' of `values` (of length one more than that of `breakpoints`) applied to the
+#' bins from smallest to largest. If specified,
+#' `values` will override `midpoint`.
 #' @param returns A finite distribution with the specified bins.
-#' @note `NA` breakpoints are silently removed.
+#' @note `NA` breakpoints and `values` (if specified) are silently removed.
+#' Will throw an error if the lengths of `values` does not end up being
+#' one more than that of `breakpoints`.
 #' @examples
 #' p1 <- dst_norm(0, 1)
 #' p2 <- discretise(p1, breakpoints = -2:2)
@@ -24,49 +30,76 @@
 #'   discretize(-2:2)
 #' @rdname discretise
 #' @export
-discretise <- function(distribution, breakpoints, midpoints = c("median", "mean"), closed = c("right", "left")) {
+discretise <- function(
+    distribution,
+    breakpoints,
+    midpoints = c("median", "mean", "label"),
+    values,
+    closed = c("right", "left")
+) {
   midpoints <- rlang::arg_match(midpoints)
   closed <- rlang::arg_match(closed)
   right_closed <- closed == "right"
   breakpoints <- sort(unique(breakpoints))
   n_break <- length(breakpoints)
-  if (n_break == 0) {
-    value <- rlang::exec(midpoints, distribution)
-    return(dst_degenerate(value))
-  }
   ## Probabilities
   cdf <- prob_left(distribution, breakpoints, inclusive = right_closed)
   cdf <- append(0, cdf)
   p <- diff(cdf)
   r <- prob_right(distribution, breakpoints[n_break], inclusive = !right_closed)
   p <- append(p, r)
-  ## Midpoints
-  slices <- list(
-    suppressWarnings(slice_right(
-      distribution, breakpoints[1L], include = !right_closed
-    ))
-  )
-  for (i in 1L + seq_len(n_break - 1L)) {
-    d <- suppressWarnings(slice_right(
-      distribution, breakpoints[i], include = !right_closed
-    ))
-    if (!is.null(d)) {
-      d <- suppressWarnings(slice_left(
-        d, breakpoints[i - 1L], include = right_closed
-      ))
-    }
-    slices[[i]] <- d
+  if (length(p) == 0) {
+    p <- 1
   }
-  slices[[n_break + 1L]] <- suppressWarnings(slice_left(
-    distribution, breakpoints[n_break], include = right_closed
-  ))
+
+  ## Midpoints
+  if (missing(values) && missing(midpoints)) {
+    stop("Must specify either `values` or `midpoints` arguments.")
+  } else if (!missing(values)) {
+    values <- values[!is.na(values)]
+    if (length(values) != length(breakpoints) + 1) {
+      stop("`values` provided are of length ", length(values),
+           ", but should have length ", length(breakpoints) + 1,
+           " (one more than `breakpoints`).")
+    }
+  } else {
+    if (length(breakpoints) == 0) {
+      values <- rlang::exec(midpoints, distribution)
+    } else {
+      slices <- list(
+        suppressWarnings(slice_right(
+          distribution, breakpoints[1L], include = !right_closed
+        ))
+      )
+      for (i in 1L + seq_len(n_break - 1L)) {
+        d <- suppressWarnings(slice_right(
+          distribution, breakpoints[i], include = !right_closed
+        ))
+        if (!is.null(d)) {
+          d <- suppressWarnings(slice_left(
+            d, breakpoints[i - 1L], include = right_closed
+          ))
+        }
+        slices[[i]] <- d
+      }
+      slices[[n_break + 1L]] <- suppressWarnings(slice_left(
+        distribution, breakpoints[n_break], include = right_closed
+      ))
+      values <- vapply(slices, function(dst) {
+        if (is.null(dst)) {
+          NA_real_
+        } else {
+          rlang::exec(midpoints, dst)
+        }
+      }, FUN.VALUE = numeric(1L))
+    }
+  }
+
   nonzero <- p > 0
   p <- p[nonzero]
-  slices <- slices[nonzero]
-  values <- vapply(slices, get(midpoints), FUN.VALUE = numeric(1L))
+  values <- values[nonzero]
   distionary::dst_empirical(values, weights = p)
 }
-
 
 #' @rdname discretise
 #' @export
--- tests/testthat/test-discretise.R
+++ tests/testthat/test-discretise.R
@@ -8,6 +8,7 @@ test_that("Base discretise functionality works", {
   expect_true(all(d1$probabilities$location[1:n_breaks] <= breaks))
   expect_true(all(d1$probabilities$location[1 + 1:n_breaks] >= breaks))
   expect_equal(d1, discretise(d0, append(breaks, NA)))
+  expect_equal(discretize(d0, -2:2), discretize(d0, 2:-2))
 })
 
 test_that("Venturing outside of the probability distribution works.", {
@@ -26,4 +27,34 @@ test_that("Open and Closed works.", {
   d12 <- discretise(d0, c(0, 4, 10), closed = "left")
   expect_equal(nrow(d11$probabilities), 4)
   expect_equal(nrow(d12$probabilities), 3)
+})
+
+test_that("Manual specification of values works.", {
+  d0 <- dst_norm(0, 1)
+  d11 <- discretize(d0, -2:2, values = -3:2)
+  d12 <- discretize(d0, -2:2)
+  expect_equal(d11$probabilities$location, -3:2)
+  expect_equal(d11$probabilities$size, d12$probabilities$size)
+  expect_equal(
+    discretise(d0, -2:2, values = 1:6),
+    discretise(d0, c(-2:2, NA), values = 1:6)
+  )
+  expect_equal(
+    discretise(d0, -2:2, values = 1:6),
+    discretise(d0, -2:2, values = c(1:6, NA))
+  )
+  expect_equal(
+    discretise(d0, -2:2, values = 1:6),
+    discretise(d0, c(-2:2, NA), values = c(1:6, NA))
+  )
+  expect_error(discretise(d0, -2:2, values = 1:2))
+  expect_error(discretise(d0, -2:2, values = 1:20))
+  expect_equal(
+    discretise(d0, -2:2, values = 1:6),
+    discretise(d0, 2:-2, values = 1:6)
+  )
+  expect_true(
+    !identical(discretise(d0, -2:2, values = 1:6),
+               discretise(d0, -2:2, values = 6:1))
+  )
 })
