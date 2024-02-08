52164bb13072655c958c656a137056452e4f0248
Vincenzo Coia <vincenzo.coia@gmail.com>
2024-02-08 12:47:08 -0800

    Fix graft NA evaluation

--- R/representations-graft.R
+++ R/representations-graft.R
@@ -3,7 +3,9 @@ eval_quantile.graft <- function(distribution, at) {
 	p_cutoff <- distribution$components$probs[1L]
 	res <- numeric(0L)
 	for (i in seq_along(at)) {
-		if (at[i] <= p_cutoff) {
+		if (is.na(at[i])) {
+			res[i] <- NA_real_
+		} else if (at[i] <= p_cutoff) {
 			new_at <- at[i] / p_cutoff
 			this_d <- distribution$components$distributions[[1L]]
 			res[i] <- distionary::eval_quantile(this_d, at = new_at)
